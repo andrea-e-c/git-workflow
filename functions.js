f93c1b192fc3e6d3d8603302a4ebf5c62a1ccf79
Andrea Childress <aesselman0830@gmail.com>
2021-12-13 16:14:29 -0500

    Created basic color changer app.

---
+++ functions.js
@@ -0,0 +1,5 @@
+function colorChanger(color){
+// console.log(document.body.style.backgroundColor)
+    document.body.style.backgroundColor = color;
+    console.log(document.body.style.backgroundColor)
+}
